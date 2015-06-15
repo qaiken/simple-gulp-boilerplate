20cc3c4880f74dbada7ca2f63229107bd2b5bb2b
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-14 19:23:55 -0500

    add gulp production task

--- _/gulp/srcs.js
+++ _/gulp/srcs.js
@@ -6,13 +6,14 @@ module.exports = {
   js: {
     all: '_/scripts/**/*.js',
     main: '_/scripts/app.js',
-    production: 'js/app.js'
+    production: 'js/app.js',
+    jsDir: 'js/'
   },
   sass: {
     all: '_/scss/**/*.scss',
     main: '_/scss/*.scss',
-    css: './css/*.css',
-    cssDir: './css/'
+    css: 'css/*.css',
+    cssDir: 'css/'
   },
   img: {
     all: '_/img/**/*.*'
--- _/gulp/tasks/compressCSS.js
+++ _/gulp/tasks/compressCSS.js
@@ -1,11 +1,11 @@
 var gulp      = require('gulp'),
-    config    = require('../srcs'),
+    srcs      = require('../srcs'),
     minifyCSS = require('gulp-minify-css'),
     combineMQ = require('gulp-combine-mq');
 
 gulp.task('compressCSS', ['sass'], function() {
-  return gulp.src(config.sass.css)
+  return gulp.src(srcs.sass.css)
     .pipe(combineMQ())
     .pipe(minifyCSS({advanced:false}))
-    .pipe(gulp.dest(config.sass.cssDir));
+    .pipe(gulp.dest(srcs.sass.cssDir));
 });
